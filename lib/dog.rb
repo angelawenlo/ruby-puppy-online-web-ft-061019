ba8ffb8294be169f0fcde760e6e75750b84e396e
Angela Lo <angelawenlo@gmail.com>
2019-06-18 17:58:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
-require 'pry'
 class Dog
   attr_accessor :name
   @@all = []
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-
-      binding.pry
-    end
-  end
-
-end
